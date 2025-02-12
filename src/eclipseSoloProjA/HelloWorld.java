eb5342ead15dceb1ea9363d21ca253db98dd9f77
harishSr21 <hsridha1@emich.edu>
2025-02-11 23:22:28 -0500

    Entirely new commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Howdy HelloWorld Project");
+		System.out.println("Hey this is the second programmer again");
 		System.out.println("Programmer 1 did this");
 		System.out.println("Hello, I'm the second guy");
 
